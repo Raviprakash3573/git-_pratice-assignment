ef6a84621f8821bfbd2e7972089969462bb55581
ujjwalara95 <ujjwalara95@gmail.com>
2022-11-06 09:43:53 +0530

    fixed

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,10 @@
 function palindrome(str){
     let rev="";
-for(let i=str.length-1;i>=0;i++){
-    rev+=str[i];
-}
-if(rev==str){
-    return true;
-}
-else{return false}
-}
+    for(let i=str.length-1;i>=0;i--){
+      rev+=str[i];
+    }
+    return rev == str;
+  }
+  let input = "suman";
+  let ans = palindrome(input);
+  ans?console.log(input,": palindrome"):console.log(input,": Not palindrome");
